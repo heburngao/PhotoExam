ff86277d72164b38b377c783f44d805b62408cbc
heburngao <langman048@aliyun.com>
2019-10-26 12:17:49 +0800

    photon client & server

---
+++ PhotoClientDotNet/ChatClientDonet.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+
+namespace PhotoClientDotNet
+{
+    public   class ChatClientDonet
+    {
+        public static void Main(string[] args)
+        {
+            PeerListener listener = new PeerListener();
+            var peer = new PhotonPeer(listener, ConnectionProtocol.Tcp);
+            peer.Connect("127.0.0.1:4530", "ChatServer");
+            while (!listener.Status)
+            {
+                peer.Service();
+            }
+
+            peer.OpCustom(1, new Dictionary<byte, object>() {{10, " hello world "}}, true);
+
+            while (true)
+            {
+                peer.Service();
+            }
+        }
+    }
+
+    public class PeerListener : IPhotonPeerListener
+    {
+        public bool Status = false;
+        public void DebugReturn(DebugLevel level, string message)
+        {
+            Console.WriteLine(" Debug Return : " +level +" : " + message);
+        }
+
+        public void OnOperationResponse(OperationResponse operationResponse)
+        {
+            switch (operationResponse.OperationCode)
+            {
+                case 2 :
+                    if (operationResponse.Parameters.TryGetValue(20, out var obj))
+                    {
+                        Console.WriteLine(" rcv : " + obj);
+                    }
+
+                    break;
+            }
+
+            
+        }
+
+        public void OnStatusChanged(StatusCode statusCode)
+        {
+            switch (statusCode)
+            {
+                case StatusCode.Connect:
+                    Status = true;
+                    Console.WriteLine(" Server Connected !");
+                    break;
+            }
+        }
+
+        public void OnEvent(EventData eventData)
+        {
+        }
+    }
+}
---
+++ PhotoServer/ChatServer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace PhotoServer
+{
+    public class ChatServer:ApplicationBase
+    {
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            var clientPeer = new ChatPeer(initRequest.Protocol,initRequest.PhotonPeer);
+            return clientPeer;
+        }
+
+        protected override void Setup()
+        {
+              
+        }
+
+        protected override void TearDown()
+        {
+             
+        }
+    }
+
+    public class ChatPeer : PeerBase
+    {
+        public ChatPeer(InitRequest initRequest) : base(initRequest)
+        {
+        }
+
+        public ChatPeer(IRpcProtocol protocol, IPhotonPeer unmanagedPeer) : base(protocol, unmanagedPeer)
+        {
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            switch (operationRequest.OperationCode)
+            {
+                case 1:
+                    if (operationRequest.Parameters.TryGetValue(10, out var obj))
+                    {
+
+
+                        SendOperationResponse(
+                            new OperationResponse(2,
+                                new Dictionary<byte, object>() {{20, operationRequest.OperationCode + "|" + obj}}),
+                            sendParameters);
+                    }
+
+                    break;
+            }
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+             
+        }
+    }
+}
