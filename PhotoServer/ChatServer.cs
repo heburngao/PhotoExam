e236d2b2ef666664f63cc2bbfa93b49a4b3a8392
heburngao <langman048@aliyun.com>
2019-10-31 20:22:28 +0800

    MySQLClientHelper log4net

--- PhotoServer/ChatServer.cs
+++ PhotoServer/ChatServer.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using ExitGames.Diagnostics.Counter;
+using ExitGames.Diagnostics.Monitoring;
+using ExitGames.Logging;
+using ExitGames.Logging.Log4Net;
+
+using log4net.Config;
 using Photon.SocketServer;
+using Photon.SocketServer.Diagnostics;
 using PhotonHostRuntimeInterfaces;
+
+
 
 namespace PhotoServer
 {
     public class ChatServer:ApplicationBase
     {
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
         protected override PeerBase CreatePeer(InitRequest initRequest)
         {
             var clientPeer = new ChatPeer(initRequest.Protocol,initRequest.PhotonPeer);
@@ -14,17 +25,41 @@ namespace PhotoServer
 
         protected override void Setup()
         {
-              
+              log4net.GlobalContext.Properties["Photon:ApplicationLogPath"] = Path.Combine(this.ApplicationRootPath, "log");
+              log4net.GlobalContext.Properties["LogFileName"] = "TD" + this.ApplicationName;
+              var file = new FileInfo(Path.Combine(this.BinaryPath, "log4net.config"));
+              if (file.Exists)
+              {
+                  LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+                  XmlConfigurator.ConfigureAndWatch(file);
+              }
+              log.InfoFormat("Create application Instance: Type={0}", Instance.GetType());
+              log.Debug("!!!!!!!!!!!!!!!!! Application setup complete");
+
+              Initialize();
+        }
+
+        private void Initialize()
+        {
+             CounterPublisher.DefaultInstance.AddStaticCounterClass(typeof(Counter),"PhotoServer");
+             Protocol.AllowRawCustomValues = true;
         }
 
         protected override void TearDown()
         {
-             
+             log.Debug("!!!!!!!!!!!!!!!!!!! Application tear down");
         }
+    }
+
+    public static class Counter
+    {
+        [PublishCounter("Photo")]
+        public static readonly NumericCounter Games = new NumericCounter("Photo");
     }
 
     public class ChatPeer : PeerBase
     {
+        private static readonly ILogger log = LogManager.GetCurrentClassLogger();
         public ChatPeer(InitRequest initRequest) : base(initRequest)
         {
         }
@@ -40,7 +75,7 @@ namespace PhotoServer
                 case 1:
                     if (operationRequest.Parameters.TryGetValue(10, out var obj))
                     {
-
+                        log.Debug("!!!!!!!!!! rcv from client ： ");
 
                         SendOperationResponse(
                             new OperationResponse(2,
@@ -54,7 +89,7 @@ namespace PhotoServer
 
         protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
         {
-             
+            log.Debug("!!!!!!!!!!!!! disconnect ");
         }
     }
 }
