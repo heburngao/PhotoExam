9f2bc84cf07ae8a09500dd2c8a6dc9c493938dbb
heburngao <langman048@aliyun.com>
2019-11-12 21:14:59 +0800

    test attribute3 xxx

--- PhotoClientDotNet/ChatClientDonet.cs
+++ PhotoClientDotNet/ChatClientDonet.cs
@@ -11,15 +11,14 @@ namespace PhotoClientDotNet
         
         public static void Main(string[] args)
         {
-           
 
-//            Type t = typeof(AA);//UseAttributeExample);
-//            var ass = t.GetCustomAttributes(typeof(SomethingAttribute), true);
-//            foreach (SomethingAttribute o in ass)
-//            { 
-//                Console.WriteLine("attribute: " +o.Name +"/"+o.Data +"/"+ o.TypeId);
-//            } 
-            toTestPhoton();
+            Type t = typeof(AA);//UseAttributeExample);
+            var ass = t.GetCustomAttributes(typeof(SomethingAttribute), true);
+            foreach (SomethingAttribute o in ass)
+            { 
+                Console.WriteLine("attribute: " +o.Name +"/"+o.Data +"/"+ o.TypeId);
+            } 
+            //toTestPhoton();
         }
 
         private static void toTestPhoton()
